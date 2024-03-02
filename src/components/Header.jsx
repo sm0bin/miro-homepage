a1b82739920fe894e9ba0ede5afa38bb81ca2372
GhostMobin <s1810677110@ru.ac.bd>
2024-03-03 00:13:36 +0600

    navbar 1

---
+++ src/components/Header.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+
+const Header = () => {
+
+    return (
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </div>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        {/* Product v Solutions v Resources v Enterprise Pricing */}
+                        <li>
+                            <a>Product</a>
+                            <ul className="p-2">
+                                <li><a>Submenu 1</a></li>
+                                <li><a>Submenu 2</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a>Solutions</a>
+                            <ul className="p-2">
+                                <li><a>Submenu 1</a></li>
+                                <li><a>Submenu 2</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a>Resources</a>
+                            <ul className="p-2">
+                                <li><a>Submenu 1</a></li>
+                                <li><a>Submenu 2</a></li>
+                            </ul>
+                        </li>
+                        <li><a>Enterprise</a></li>
+                        <li><a>Pricing</a></li>
+
+                    </ul>
+                </div>
+                <a className="btn btn-ghost text-xl">daisyUI</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <li>
+                        <details>
+                            <summary>Product</summary>
+                            <ul className="p-2">
+                                <li><a>Submenu</a></li>
+                                <li><a>Submenu</a></li>
+                            </ul>
+                        </details>
+                    </li>
+                    <li>
+                        <details>
+                            <summary>Solutions</summary>
+                            <ul className="p-2">
+                                <li><a>Submenu</a></li>
+                                <li><a>Submenu</a></li>
+                            </ul>
+                        </details>
+                    </li>
+                    <li>
+                        <details>
+                            <summary>Resources</summary>
+                            <ul className="p-2">
+                                <li><a>Submenu</a></li>
+                                <li><a>Submenu</a></li>
+                            </ul>
+                        </details>
+                    </li>
+                    <li><a>Enterprise</a></li>
+                    <li><a>Pricing</a></li>
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a className="btn">Button</a>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
